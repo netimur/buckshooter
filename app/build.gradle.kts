727bde06d29ea4c58a96edcb56699e73a7479435
Timur Kaliev <TKaliev@one.kz>
2024-11-17 23:04:19 +0500

    Add Kotlin Symbol Processing tool

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.ksp)
 }
 
 android {
